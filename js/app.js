534046a9a6e50e9f9e4eef47be2a1b0ae35cb392
matts-github <mattwoodhouse@live.co.uk>
2022-02-18 13:46:22 +0000

    html, sass and js update

--- js/app.js
+++ js/app.js
@@ -37,93 +37,152 @@ function show() {
 
 // Form validation
 
-const fname = document.getElementById('first-name')
-const lname = document.getElementById('last-name')
-const email = document.getElementById('email')
-const subject = document.getElementById('subject')
-const message = document.getElementById('message')
-const form = document.getElementById('contact-form')
-const errorElement = document.getElementsByClassName('error')
+// const fname = document.getElementById('first-name')
+// const lname = document.getElementById('last-name')
+// let email = document.getElementById('email').value;
+// const subject = document.getElementById('subject')
+// const message = document.getElementById('message')
+// const form = document.getElementById('contact-form')
+// const errorElement = document.getElementsByClassName('error')
+const isValidEmail = (email) => {
+    const re = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    return re.test(String(email).toLowerCase());
+} 
 
 
+const form = document.querySelector('form');
+const thankYou = document.querySelector('.thank-you');
+const nameInput = document.querySelector(
+    'input[name="first-name"]'
+);
+const emailInput = document.querySelector(
+    'input[name="email"]'
+);
 
+let isFormValid = false;
 
-form.addEventListener('submit', (e) => {
-    let messages = []
-    if (message.value === '' || message.value == null) {
-        messages.push('Message is required')
-    }
-
-    if (messages.length > 0) {
-    e.preventDefault()
-    errorElement[4].innerText = messages.join(', ')
-    }
-})
-form.addEventListener('submit', (e) => {
-    let messages = []
-    if (subject.value === '' || subject.value == null) {
-        messages.push('Subject is required')
-    }
-
-    if (messages.length > 0) {
-    e.preventDefault()
-    errorElement[3].innerText = messages.join(', ')
-    }
-})
-form.addEventListener('submit', (e) => {
-    let messages = []
-    if (email.value === '' || email.value == null) {
-        messages.push('Email is required')
-    }
-
-    if (messages.length > 0) {
-    e.preventDefault()
-    errorElement[2].innerText = messages.join(', ')
-    }
-})
-form.addEventListener('submit', (e) => {
-    let messages = []
-    if (lname.value === '' || lname.value == null) {
-        messages.push('Last Name is required')
-    }
-
-    if (messages.length > 0) {
-    e.preventDefault()
-    errorElement[1].innerText = messages.join(', ')
-    }
-})
-form.addEventListener('submit', (e) => {
-    let messages = []
-    if (fname.value === '' || fname.value == null) {
-        messages.push('First Name is required')
-    }
-
-    if (messages.length > 0) {
-    e.preventDefault()
-    errorElement[0].innerText = messages.join(', ')
-    }
-})
-
-// Regex check
-function validation()
-{
-
-    var email = document.getElementById("email").value;
-    var pattern = /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9])+.([a-z]+)(.[a-z]+)?$/;
-
-    if (email.match(pattern))
-    {
-        form.classList.add("valid")
-        form.classList.remove("invalid")
-        text.innerHTML = "Your Email Address in Valid.";
-        text.style.color = "#00ff00";
-    }
-    else
-    {
-        form.classList.remove("valid")
-        form.classList.add("invalid")
-        text.innerHTML = "Please Enter Valid Email Address.";
-        text.style.color = "#ff0000";
-    }
+const resetElm = (elm) => {
+    elm.classList.remove('invalid');
+    elm.nextElementSibling.classList.add("hidden");
 }
+
+const invalidateElm = (elm) => {
+    nameInput.classList.add("invalid");
+    nameInput.nextElementSibling.classList.remove("hidden");
+}
+
+const validateInputs = () => {
+    isFormValid = true;
+    resetElm(nameInput);
+    resetElm(emailInput);
+
+    if (!nameInput.value) {
+        isFormValid = false;
+        invalidateElm(nameInput);
+    }
+
+    if (!isValidEmail(emailInput.value)) {
+        isFormValid = false;
+        console.log("hello");
+        invalidateElm(emailInput);
+    }
+};
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    validateInputs();
+    if (isFormValid) {
+        form.remove();
+        thankYou.classList.remove("hidden");
+    }
+});
+
+nameInput.addEventListener('input', () => {
+    validateInputs();
+});
+
+
+
+
+
+
+
+
+
+//     if (pattern.test(email) == false) {
+//         //stop from working
+//         return false;
+//     }else{
+
+//         form.addEventListener('submit', (e) => {
+//             let messages = []
+//             if (message.value === '' || message.value == null) {
+//                 messages.push('Message is required')
+//             }
+
+//             if (messages.length > 0) {
+//             e.preventDefault()
+//             errorElement[4].innerText = messages.join(', ')
+//             }
+//         })
+//         form.addEventListener('submit', (e) => {
+//             let messages = []
+//             if (subject.value === '' || subject.value == null) {
+//                 messages.push('Subject is required')
+//             }
+
+//             if (messages.length > 0) {
+//             e.preventDefault()
+//             errorElement[3].innerText = messages.join(', ')
+//             }
+//         })
+//         form.addEventListener('submit', (e) => {
+            
+//         })
+//         form.addEventListener('submit', (e) => {
+//             let messages = []
+//             if (lname.value === '' || lname.value == null) {
+//                 messages.push('Last Name is required')
+//             }
+
+//             if (messages.length > 0) {
+//             e.preventDefault()
+//             errorElement[1].innerText = messages.join(', ')
+//             }
+//         })
+//         form.addEventListener('submit', (e) => {
+//             let messages = []
+//             if (fname.value === '' || fname.value == null) {
+//                 messages.push('First Name is required')
+//             }
+
+//             if (messages.length > 0) {
+//             e.preventDefault()
+//             errorElement[0].innerText = messages.join(', ')
+//             }
+//         })
+//     }
+
+// // Regex check
+// function validation()
+// {
+
+//     var email = document.getElementById("email").value;
+    
+
+//     if (email.match(pattern))
+//     {
+//         form.classList.add("valid")
+//         form.classList.remove("invalid")
+//         text.innerHTML = "Your Email Address in Valid.";
+//         text.style.color = "#00ff00";
+//     }
+//     else
+//     {
+//         form.classList.remove("valid")
+//         form.classList.add("invalid")
+//         text.innerHTML = "Please Enter Valid Email Address.";
+//         text.style.color = "#ff0000";
+//     }
+// }
 
